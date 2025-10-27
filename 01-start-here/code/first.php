d7804580582d9fb75807740bae5ef81e46228bd3
benj-lazaro <benjie.work@gmail.com>
2025-10-27 18:50:28 +0800

    01 start here: your first program in 10 minutes

---
+++ 01-start-here/code/first.php
@@ -0,0 +1,13 @@
+<?php
+
+// Declare variables & assign initial values
+$name = "Fellow Coder";
+$message = "The best way to predict the future is to create it.";
+
+// Display values of variable within a string of text
+echo "=======\n";
+echo "Hello, $name!\n";
+echo "\n";
+echo "Here is your thought for today: \n";
+echo "$message\n";
+echo "=======\n";
